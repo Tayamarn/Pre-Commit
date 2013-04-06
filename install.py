4dc7f9d19710a0755927552cd59109598087ac5b
tayamarn <max@goldenforests.ru>
2013-04-07 00:31:31 +0400

    v 0.3

--- install.py
+++ install.py
@@ -5,17 +5,14 @@ import Tkinter, tkFileDialog, tkMessageBox
 import filecmp
 import unittest
 
-src = os.getcwd() + '\pre-commit'
+src = os.getcwd() + '/pre-commit'
 dst = ''
 
 def getDestinationPathInteractive():
     print('Specify your repository directory:')
     path = raw_input()
-    #path = 'E:/test1/.git/hooks'
-    #path = path.replace('\\', '/')
     if not os.path.isdir(path):
         print("The specified path doesn't exist.")
-        raw_input()
         sys.exit(1) 
     else:
         return path
@@ -46,17 +43,15 @@ def checkIfPathValid(path):
             tkMessageBox.showerror('Error', errorMsg)
         else:
             print(errorMsg)
-            raw_input()
         sys.exit(1)        
         
 def fileExists(dst):
-    if filecmp.cmp(os.getcwd() + '\pre-commit', dst + '/hooks/pre-commit'):
+    if filecmp.cmp(os.getcwd() + '/pre-commit', dst + '/hooks/pre-commit'):
         infoMsg = "The specified repository already contains the hook."
         if os.name == 'nt':
             tkMessageBox.showinfo('', infoMsg)
         else:
             print(infoMsg)
-            raw_input()
         sys.exit(0)
     else:
         yesnoMsg = "The specified repository already contains some pre-commit hook. Do you want to overwrite it?"
@@ -76,17 +71,14 @@ def fileExists(dst):
             if raw_input().lower()[0] == 'y':
                 shutil.copyfile(src, dst + '/hooks/pre-commit')
                 print(successMsg)
-                raw_input()
                 sys.exit(0)
             else:
-                print(failMsg)
-                raw_input()                
+                print(failMsg)           
                 sys.exit(1)      
 
 def printHelp():
     print('usage: install.py [<path>]')
     print('<path> is the path to git repository, where you want the hook to be installed')
-    raw_input()
     sys.exit(1)        
    
    
@@ -119,7 +111,6 @@ else:
         tkMessageBox.showinfo('', infoMsg)
     else:
         print(infoMsg)
-        raw_input()
     sys.exit(0)
     
     
